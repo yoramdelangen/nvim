a01c2ef529530cca1680fe78d5de6a2b309f7853
Yoram <yoramdelangen@gmail.com>
2024-07-09 11:09:35 +0200

    added lsp-attach and missing commands

--- lua/yoram/set.lua
+++ lua/yoram/set.lua
@@ -34,3 +34,27 @@ vim.opt.colorcolumn = "80"
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+vim.opt.clipboard = "unnamed,unnamedplus"
+
+-- commands
+-- Make sure we can always exit properly!
+local command = vim.api.nvim_create_user_command
+command("WQ", "wq", {})
+command("Wq", "wq", {})
+command("W", "w", {})
+command("Q", "q", {})
+
+-- Create a command `:Format` local to the LSP buffer
+command("Format", function(args)
+    -- vim.lsp.buf.format()
+    local range = nil
+    if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+        }
+    end
+    require("conform").format({ async = false, lsp_fallback = true, range = range })
+end, { range = true, desc = "Format current buffer with LSP" })
