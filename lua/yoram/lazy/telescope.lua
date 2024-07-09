22edc8f5a45c98ee5346bab940cfc005ba8c0124
Yoram <yoramdelangen@gmail.com>
2024-07-09 11:10:11 +0200

    config theming and add missing keymap telescope

--- lua/yoram/lazy/telescope.lua
+++ lua/yoram/lazy/telescope.lua
@@ -36,5 +36,17 @@ return {
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+        vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles,
+            { desc = "[?] Find recently opened files" })
+        vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers,
+            { desc = "[ ] Find existing buffers" })
+        vim.keymap.set("n", "<leader>/", function()
+            -- You can pass additional configuration to telescope to change theme, layout, etc.
+            require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                winblend = 10,
+                previewer = false,
+            }))
+        end, { desc = "[/] Fuzzily search in current buffer" })
     end
 }
